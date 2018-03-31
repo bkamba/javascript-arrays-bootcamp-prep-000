212cb797e3831c729292b10d5e68540d81464841
Bill Kamba <billkamba95@gmail.com>
2018-03-31 17:22:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function removeElementFromBeginningOfArray(array) {
 
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  array.slice(array.length - 1)
+  array.shift()
   return array
 }
 
