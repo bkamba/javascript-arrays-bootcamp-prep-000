88aa6bb4363be9b28d4a5a6bf5bd9d22ee62e415
Bill Kamba <billkamba95@gmail.com>
2018-03-31 17:02:12 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,46 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, ele){
+  var arr = [ele, ...array]
+  return arr
+} 
+
+
+function destructivelyAddElementToBeginningOfArray(array, ele)
+{
+  array.unshift(ele);
+  return array;
+  
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  
+  array.shift();
+  return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  var lessChocalate = array.slice(0)
+  return lessChocalate;
+}
+
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.slice(array.length - 1)
+  return array
+}
+
+function removeElementFromEndOfArray(array) {
+  var less = array.slice(array.length - 1)
+  return less
+}
+
+
+
+
+
+
